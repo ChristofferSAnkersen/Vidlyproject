8f3d1b80ca15808be963dcb564e078605bf549f9
Christoffer Sohn Ankersen <chri5122@edu.eal.dk>
2018-04-19 08:18:02 +0200

    2nd commit.

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -13,6 +13,16 @@ namespace Vidly
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+
+            routes.MapMvcAttributeRoutes();
+
+            routes.MapRoute(
+                "MoviesByReleaseDate",
+                "movies/released/{year}/{month}",
+                new { controller = "Movies", action = "ByReleaseDate", },
+                //new { year = "\\d{4}", month = @"\d{2}" }, //This does the exact same thing
+                new { year = @"2015|2016", month = @"\d{2}" }); //Contraint on the year and month
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
